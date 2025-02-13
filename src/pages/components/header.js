7f65feb68827509a59611001ea19a79ffa4f8e82
Abila03 <abilan131@gmail.com>
2025-02-13 10:17:09 +0700

    header 1.2.3.2

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -8,18 +8,6 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container px-5">
-        <Link className="navbar-brand" to="/">Interworkz</Link>
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarSupportedContent" 
-          aria-controls="navbarSupportedContent" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
