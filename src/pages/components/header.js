eac720bcbd6c7848f3cdc7def4463e96ea9a27fc
Abila03 <abilan131@gmail.com>
2025-06-28 20:06:18 +0700

    header updae

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom"; // Import useLocation
+import { Link, useLocation } from "react-router-dom";
 import "../style.css";
 
 function Header() {
-  const location = useLocation(); // Dapatkan URL saat ini
+  const location = useLocation();
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -11,6 +11,20 @@ function Header() {
         <Link className="navbar-brand fw-bold text-white" to="/">
           Interworkz
         </Link>
+
+        {/* Toggler button (untuk tampilan mobile) */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
@@ -21,13 +35,13 @@ function Header() {
             </li>
             <li className="nav-item">
               <Link className={`nav-link ${location.pathname === "/service" ? "active" : ""}`} to="/service">Services</Link>
-            </li>             
+            </li>
             <li className="nav-item">
               <Link className={`nav-link ${location.pathname === "/carrers" ? "active" : ""}`} to="/carrers">Carrers</Link>
             </li>
             <li className="nav-item">
               <Link className={`nav-link ${location.pathname === "/contact" ? "active" : ""}`} to="/contact">Contact</Link>
-            </li>                            
+            </li>
           </ul>
         </div>
       </div>
