716566d843a883ca0d43f2684ce095dde32f3dd5
Abila03 <abilan131@gmail.com>
2025-06-28 10:51:53 +0700

    update footer

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -11,50 +11,6 @@ function Footer() {
         <p className="m-0 text-center text-white">
           2025 © Copyright - Interworkz Technology. All rights reserved
         </p>
-        <div className="navbar-collapse ">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <p className="nav-item text-white">
-              <Link
-                className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
-                to="/"
-              >
-                Home
-              </Link>
-            </p>
-            <p className="nav-item text-white">
-              <Link
-                className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`}
-                to="/about"
-              >
-                About
-              </Link>
-            </p>
-            <p className="nav-item text-white">
-              <Link
-                className={`nav-link ${location.pathname === '/service' ? 'active' : ''}`}
-                to="/service"
-              >
-                Services
-              </Link>
-            </p>
-            <p className="nav-item text-white">
-              <Link
-                className={`nav-link ${location.pathname === '/careers' ? 'active' : ''}`}
-                to="/careers"
-              >
-                Careers
-              </Link>
-            </p>
-            <p className="nav-item text-white">
-              <Link
-                className={`nav-link ${location.pathname === '/contact' ? 'active' : ''}`}
-                to="/contact"
-              >
-                Contact
-              </Link>
-            </p>
-          </ul>
-        </div>
       </div>
     </footer>
   );
