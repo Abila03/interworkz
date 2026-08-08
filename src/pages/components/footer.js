ae6f3cda0789f5c1a2d20954c5cb317ef2d84907
Abila03 <abilan131@gmai.com>
2026-08-08 17:16:25 +0700

    update

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -7,11 +7,33 @@ function Footer() {
 
   return (
     <footer className="py-3 bg-primary">
-      <div className="container">
-        <p className="m-0 text-center text-white">
+      
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container justify-content-center text-center text-white">
           2026 © Copyright - Interworkz Technology. All rights reserved
-        </p>
-      </div>
+        </div>
+        <div className="container px-5">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent"> 
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname.startsWith("/service") ? "active" : ""}`} to="/service">Services</Link>
+              </li>
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname === "/carrers" ? "active" : ""}`} to="/carrers">Carrers</Link>
+              </li>
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname === "/contact" ? "active" : ""}`} to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </footer>
   );
 }
