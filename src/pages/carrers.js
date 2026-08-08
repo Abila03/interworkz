86542aedd189e87a802e018d6db4ebc3012549ae
Abila03 <abilan131@gmai.com>
2026-08-08 16:35:32 +0700

    update carrers.js

--- src/pages/carrers.js
+++ src/pages/carrers.js
@@ -1,11 +1,12 @@
 import Header from './components/header';
 import Footer from './components/footer';
 import Head from './components/head';
+import HeaderCarrers from './components/carrers/header';
 
 function Carrers() {
   return (
     <div>
-      <HeaderService />
+      <HeaderCarrers />
     </div>
   );
 }
