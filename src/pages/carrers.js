73660f5d14af9c82e9ad0c89a4e825f02f4fef69
Abila03 <abilan131@gmai.com>
2026-08-08 16:50:05 +0700

    update carrers.js contact.js

--- src/pages/carrers.js
+++ src/pages/carrers.js
@@ -1,6 +1,3 @@
-import Header from './components/header';
-import Footer from './components/footer';
-import Head from './components/head';
 import HeaderCarrers from './components/carrers/header';
 
 function Carrers() {
